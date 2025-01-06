63d2202f2dc0b48514fa2070b7d299924b184096
kimseonguk197 <ksg39412@naver.com>
2025-01-06 22:16:03 +0900

    fix

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -62,6 +62,13 @@ git revert 기존의커밋ID
 
 # 작업중인 사항을 임시저장(충돌상황에서 많이 사용)
 git stash
-
-# 저장한 최신의 작업목록을 꺼내기
+# 저장한 최신의 작업목록을 꺼내서 적용하기기
 git stash pop
+# 임시저장한 목록은 그대로 놔둔채 특정 index항목 적용
+git stash apply 인덱스번호
+# 임시저장한 작업 내용 조회
+git stash show -p 인덱스번호
+# 임시저장한 작업목록조회
+git stash list
+# 저장목록 모두 삭제
+git stash clear
